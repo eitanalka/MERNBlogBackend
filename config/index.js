bc65c160dc24f816e06f65e8d67554864b249aea
Eitan Alkaslassy <eitanalka@gmail.com>
2017-04-01 17:47:59 -0400

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,1 @@
-//import configValues from './config';
 
-const getDbConnectionString = () => {
-    return `mongodb://mern-blog:mern-blog@ds145790.mlab.com:45790/mern-blog`;
-};
-
-export default getDbConnectionString;
