4c822c33f96f777226e1f1f663ecd42928a4979e
Eitan Alkaslassy <eitanalka@gmail.com>
2017-04-01 17:39:40 -0400

    change

---
+++ dist/config/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+//import configValues from './config';
+
+var getDbConnectionString = function getDbConnectionString() {
+    return "mongodb://mern-blog:mern-blog@ds145790.mlab.com:45790/mern-blog";
+};
+
+exports.default = getDbConnectionString;
+//# sourceMappingURL=index.js.map
---
+++ server/config/index.js
@@ -0,0 +1,7 @@
+//import configValues from './config';
+
+const getDbConnectionString = () => {
+    return `mongodb://mern-blog:mern-blog@ds145790.mlab.com:45790/mern-blog`;
+};
+
+export default getDbConnectionString;
